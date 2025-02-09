668f19b63709d93ea40ce066d744d79b9a5ffd16
Xtopuknul <xto.puknul@gmail.com>
2025-02-09 21:41:05 +0100

    first commit

---
+++ scripts/App.js
@@ -0,0 +1,39 @@
+const gallery = document.querySelector('.gallery');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let isDragging = false;
+let startX;
+let scrolLeft;
+
+function scrollGallery(direction) {
+    const itemWidth = document.querySelector('.gallery-item').offsetWidth + 10;
+    gallery.scrollBy({ left: direction * itemWidth, behavior: "smooth"});
+}
+prevBtn.addEventListener('click', () => scrollGallery(-1));
+nextBtn.addEventListener('click', () => scrollGallery(1));
+
+gallery.addEventListener('mousedown', (e) => {
+    isDragging = true; 
+    startX = e.pageX - gallery.offsetLeft;
+    scrolLeft = gallery.scrollLeft;
+    gallery.style.cursor = 'grabbing'
+});
+
+gallery.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - gallery.offsetLeft;
+    const walk = (x - startX) * 2;
+    gallery.scrollLeft = scrolLeft - walk;
+});
+
+gallery.addEventListener('mouseup', () => {
+    isDragging = false;
+    gallery.style.cursor = "grab";
+});
+
+gallery.addEventListener('mouseleave', () => {
+    isDragging = false;
+    gallery.style.cursor = "grab";
+});
