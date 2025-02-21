0a7dd1f92707e72547f739a1320884684cdd48c0
Xtopuknul <xto.puknul@gmail.com>
2025-02-21 13:36:54 +0100

    Very much new elements: burger, style, some new in script and html

--- scripts/App.js
+++ scripts/App.js
@@ -37,3 +37,14 @@ gallery.addEventListener('mouseleave', () => {
     isDragging = false;
     gallery.style.cursor = "grab";
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.getElementById("burger-menu");
+    const menu = document.getElementById("nav-menu");
+
+    burger.addEventListener("click", function () {
+        menu.classList.toggle("active"); // Показываем меню
+        burger.classList.toggle("active"); // Поворачиваем бургер
+        burger.textContent = menu.classList.contains("active") ? "✖" : "☰"; // Меняем иконку
+    });
+});
